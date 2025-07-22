2cc38991bd42af800281fc12d359784d9a5b4bb7
Jamal Williams <jwill702@charlotte.edu>
2025-07-22 13:39:53 -0400

    feat: added customers model

---
+++ api/models/customers.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DECIMAL, DATETIME
+from sqlalchemy.orm import relationship
+from datetime import datetime
+from ..dependencies.database import Base
+
+class OrderDetail(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    customer_name = Column(String(100))
+    customer_email = Column(String(100))
+    customer_phone = Column(Integer, index=True, nullable=False)
+    customer_address = Column(String(100))
+
+    sandwich = relationship("Sandwich", back_populates="order_details")
+    order = relationship("Order", back_populates="order_details")
