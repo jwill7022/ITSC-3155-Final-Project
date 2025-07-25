ebffb7eff4d0cdcba68c41c4e058dbf0470c386f
Devin Turner <devinturner@me.com>
2025-07-25 14:40:51 -0400

    feat: add basic code for menu items

--- api/routers/menu_items.py
+++ api/routers/menu_items.py
@@ -1,32 +1,32 @@
 from fastapi import APIRouter, Depends, FastAPI, status, Response
 from sqlalchemy.orm import Session
-from ..controllers import menu_item_resources as controller
-from ..schemas import menu_item_resources as schema
+from ..controllers import menu_items as controller
+from ..schemas import menu_items as schema
 from ..dependencies.database import engine, get_db
 
 router = APIRouter(
-    tags=['Menu Item Resources'],
-    prefix="/menu_item_resources",
+    tags=['Menu Items'],
+    prefix="/menu_items",
 )
 
 
-@router.post("/", response_model=schema.MenuItemResource)
-def create(request: schema.MenuItemResourceCreate, db: Session = Depends(get_db)):
+@router.post("/", response_model=schema.MenuItems)
+def create(request: schema.MenuItemsCreate, db: Session = Depends(get_db)):
     return controller.create(db=db, request=request)
 
 
-@router.get("/", response_model=list[schema.MenuItemResource])
+@router.get("/", response_model=list[schema.MenuItems])
 def read_all(db: Session = Depends(get_db)):
     return controller.read_all(db)
 
 
-@router.get("/{item_id}", response_model=schema.MenuItemResource)
+@router.get("/{item_id}", response_model=schema.MenuItems)
 def read_one(item_id: int, db: Session = Depends(get_db)):
     return controller.read_one(db, item_id=item_id)
 
 
-@router.put("/{item_id}", response_model=schema.MenuItemResource)
-def update(item_id: int, request: schema.MenuItemResourceUpdate, db: Session = Depends(get_db)):
+@router.put("/{item_id}", response_model=schema.MenuItems)
+def update(item_id: int, request: schema.MenuItemsUpdate, db: Session = Depends(get_db)):
     return controller.update(db=db, request=request, item_id=item_id)
 
 
