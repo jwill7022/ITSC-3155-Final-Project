af6a644821725c123d0f648ebbd0a977c7d3c0db
Devin Turner <devinturner@me.com>
2025-07-25 14:34:55 -0400

    feat: add basic code for menu item ingredients

--- api/models/menu_item_resources.py
+++ api/models/menu_item_resources.py
@@ -10,7 +10,7 @@ class MenuItemResource(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     menu_item_id = Column(Integer, ForeignKey("menu_items.id"))
     resource_id = Column(Integer, ForeignKey("resources.id"))
-    amount = Column(Integer, index=True, nullable=False, server_default='0.0')
+    amount = Column(Integer, nullable=False, server_default='0.0')
 
     menu_item = relationship("MenuItem", back_populates="menu_item_resources")
     resource = relationship("Resource", back_populates="menu_item_resources")
--- api/routers/index.py
+++ api/routers/index.py
@@ -1,7 +1,8 @@
-from . import orders, order_details, resources
+from . import orders, order_details, resources, menu_item_resources
 
 
 def load_routes(app):
     app.include_router(orders.router)
     app.include_router(order_details.router)
     app.include_router(resources.router)
+    app.include_router(menu_item_resources.router)
