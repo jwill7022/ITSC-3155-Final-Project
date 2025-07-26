287d03c507e3d56ff6b6732bc479a61c2b62d84a
Devin Turner <devinturner@me.com>
2025-07-25 20:29:23 -0400

    refactor: use pydantic field to restrict reviews

--- api/schemas/reviews.py
+++ api/schemas/reviews.py
@@ -1,11 +1,11 @@
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 
 class ReviewsBase(BaseModel):
     customer_name: str
-    rating: int
+    rating: int = Field(5, ge=1, le=5, description="Rating from 1 to 5")
     review_text: Optional[str] = None
 
 
