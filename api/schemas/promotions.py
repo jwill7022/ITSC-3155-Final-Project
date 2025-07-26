623706ddf844b6b130ca5d63257b1cfe55cf5f3f
Devin Turner <devinturner@me.com>
2025-07-26 10:29:08 -0400

    feat: add basic code for promotions

--- api/schemas/promotions.py
+++ api/schemas/promotions.py
@@ -7,12 +7,15 @@ class PromotionBase(BaseModel):
     code: str
     description: Optional[str] = None
     discount_percent: int = Field(..., ge=1, le=100, description="Percentage from 1 to 100")
-    expiration_date: Optional[str] = None
+    expiration_date: Optional[datetime] = None
 
 
 class PromotionCreate(PromotionBase):
     pass
 
+class PromotionUpdate(BaseModel):
+    discount_percent: int = Field(..., ge=1, le=100, description="Percentage from 1 to 100")
+    expiration_date: Optional[datetime] = None
 
 class Promotion(PromotionBase):
     id: int
