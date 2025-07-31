862e95ca151d4fcc3d2a5ebec4564190a7b0c245
Jamal Williams <jwill702@charlotte.edu>
2025-07-31 18:01:09 -0400

    fix: preventing potential type confusion

--- api/models/menu_item_ingredients.py
+++ api/models/menu_item_ingredients.py
@@ -10,7 +10,7 @@ class MenuItemIngredient(Base):
     id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     menu_item_id = Column(Integer, ForeignKey("menu_items.id"))
     resource_id = Column(Integer, ForeignKey("resources.id"))
-    amount = Column(Integer, nullable=False, server_default='0.0')
+    amount = Column(Integer, nullable=False, server_default='0')
 
     menu_item = relationship("MenuItem", back_populates="menu_item_ingredients")
     resource = relationship("Resource", back_populates="menu_item_ingredients")
