db5b963f26b3464fc86a3365fb75e9729e9f062b
Jamal Williams <jwill702@charlotte.edu>
2025-08-01 14:29:36 -0400

    feat: staff_actions router update
    staff_actions router now houses inventory management, order management, and revenue reporting routers.

--- api/dependencies/config.py
+++ api/dependencies/config.py
@@ -6,6 +6,6 @@ class conf:
     db_name = os.getenv("DB_NAME", "restaurant_order_system")
     db_port = int(os.getenv("DB_PORT", "3306"))
     db_user = os.getenv("DB_USER", "root")
-    db_password = os.getenv("DB_PASSWORD", "rootroot")
+    db_password = os.getenv("DB_PASSWORD", "T3nq@289vb")
     app_host = os.getenv("APP_HOST", "localhost")
     app_port = int(os.getenv("APP_PORT", "8000"))
